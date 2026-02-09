d003e6dea0b8ff486e49d23b113a80b3ac12fa49
Aimar <aimarsandoval123@gmail.com>
2026-02-09 10:23:03 +0100

    Operaciones potencia y modulo

--- src/calculadora12/Calculadora12ej.java
+++ src/calculadora12/Calculadora12ej.java
@@ -5,4 +5,14 @@ public class Calculadora12ej {
 		public int sumar(int a, int b) {
 			return a + b;
 		}
+		public int potencia(int base, int exponente) {
+			 int resultado = 1;
+			 for (int i = 0; i < exponente; i++) {
+				 resultado *= base;
+			 }
+			 return resultado;
+		}
+		public int modulo(int a, int b) {
+			 return a % b;
+		}
 }
